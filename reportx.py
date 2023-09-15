8c8d957b6b3e2c010ca497c7156619c54691e705
TAYLOR  DAMACENO <taylordamaceno@gmail.com>
2023-09-15 10:38:14 -0300

    json, script
    Script usando api GPT3 analisará um demonstrativo financeiro de uma empresa criando um relatório com sugestões de melhoria. 
    
    Dependências:
    pip install json
    pip install openai
    
    
    
    Estrutura do script:
    
    
    # Carregar os dados financeiros do arquivo JSON
    
    # Converter os dados financeiros para uma string formatada
    
    # Configurar a chave da API
    
    # Fazer a chamada à API para gerar um resumo executivo
    
    # gerando o relatório e gravando no arquivo resumoexecutivo.txt
    with open("resumoexecutivo.txt", "w") as f:
        f.write(response.choices[0].text.strip())

---
+++ reportx.py
@@ -0,0 +1,23 @@
+import json
+import openai
+
+# Carregar os dados financeiros do arquivo JSON
+with open("financeirorelatorio.json", "r") as f:
+    financial_data = json.load(f)
+
+# Converter os dados financeiros para uma string formatada
+financial_data_str = "\n".join([f"{key}: {value}" for key, value in financial_data.items()])
+
+# Configurar a chave da API
+openai.api_key = "your app key"
+
+# Fazer a chamada à API para gerar um resumo executivo
+response = openai.Completion.create(
+  engine="text-davinci-002",
+  prompt=f"crie um relatório financeiro em forma de texto na linguua portuguesa(br)  que análise de forma critica e propositiva os numeros que estou disponibilizando propondo algumas acoes para melhoria  based on the following financial data:\n{financial_data_str}",
+  max_tokens=3000
+)
+
+# Exibir o resumo gerado
+with open("resumoexecutivo.txt", "w") as f:
+    f.write(response.choices[0].text.strip())
